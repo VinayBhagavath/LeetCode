c58da8a82c4f53283fe69b8ed0f080e2d0f39e5b
VinayBhagavath <123284895+VinayBhagavath@users.noreply.github.com>
2023-02-25 16:01:45 -0800

    Add files via upload

---
+++ ConcatenationOfArray.java
@@ -0,0 +1,14 @@
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length*2];
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+        }
+        int count = 0;
+        for(int i = nums.length; i < ans.length; i++) {
+            ans[i] = nums[count];
+            count++;
+        }
+        return ans;
+    }
+}
